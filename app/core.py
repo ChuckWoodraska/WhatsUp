0f1144daeb97beed00ff0a964771bc084f79c4c4
Chuck Woodraska <chuck.woodraska@gmail.com>
2021-03-03 09:51:15 -0500

    Figuring out how to make it more usable.

--- app/core.py
+++ app/core.py
@@ -1,28 +1,50 @@
 from flask import Blueprint, render_template
 from chuck_pyutils import core as utils
 import paramiko
+import json
+import operator as ops
 
+def read_service_file():
+    with open('services.json') as f:
+        data = json.load(f)
+    # print(data)
+    return data
 
 def get_data():
-    client = paramiko.SSHClient()
-    client.get_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # client = paramiko.SSHClient()
+    # client.get_host_keys()
+    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     result = []
-    config = utils.read_config(utils.get_file_path(__file__, "services.ini"))
-    for x in config.sections():
-        client.connect(
-            config[x]["HOSTNAME"],
-            username=config[x]["USERNAME"],
-            key_filename=config[x]["KEY_FILE_PATH"],
-        )
-        for s in config[x]["SERVICES"].split(","):
-            stdin, stdout, stderr = client.exec_command(
-                "sudo service {} status".format(s)
-            )
-            temp_dict = {"name": x, "service": s, "log": [line for line in stdout]}
-            result.append(temp_dict)
-        client.close()
+    config = read_service_file()
+    # print(config)
+    for x in config:
+        print(x)
+    #     client.connect(
+    #         config["hostname"],
+    #         username=config[["username"],
+    #         key_filename=config["key_file_path"],
+    #     )
+        for s in x["services"]:
+    #         stdin, stdout, stderr = client.exec_command(
+    #             "{} service {} status".format(sudo_var, s)
+    #         )
+            print(s)
+            sudo_var = ""
+            if s["sudo"]:
+                sudo_var = "sudo "
+            print("{}{}".format(sudo_var, s['command']))
+            log = [line for line in stdout]
+    #         temp_dict = {"name": x, "service": s, "log": [line for line in stdout]}
+    #         result.append(temp_dict)
+    #     client.close()
+    # for x in ["service1", "service2"]:
+    #     temp_dict = {"name": x,
+    #                     "commands": {}}
+    #     for s in ["command1", "command2"]:
+    #         temp_dict["commands"] = {"name": s, "result": ""}
+    #     result.append(temp_dict)
+    print(result)
     return result
 
 
@@ -37,4 +59,7 @@ def index():
     :rtype:
     """
     return render_template("index.html", services=get_data())
+
+if __name__ == '__main__':
+    get_data()
 
